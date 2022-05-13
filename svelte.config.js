d83aef1ea2d742d82dd9b1da13ecda29e81c5d9f
Adrien Milcent <adrien.milcent@outlook.com>
2022-05-13 16:06:54 +0200

    update website

--- svelte.config.js
+++ svelte.config.js
@@ -1,15 +1,11 @@
 import adapter from '@sveltejs/adapter-static';
 
-const dev = "production" === "development";
-
-
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
 		adapter: adapter({
 			pages: 'docs',
-			assets: 'docs',
-			fallback: null
+			assets: 'docs'
 		})
 	}
 };
