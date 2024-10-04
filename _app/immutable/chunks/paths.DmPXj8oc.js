e0ff6dc0108fd412cd79b5321a83d648c112bb07
DrArtemi <grinalp@proton.me>
2024-10-04 19:06:44 +0200

    Updates

--- _app/immutable/chunks/paths.DmPXj8oc.js
+++
@@ -1,1 +0,0 @@
-var s;const t=((s=globalThis.__sveltekit_16bkiri)==null?void 0:s.base)??"/drartemi.github.io";var i;const a=((i=globalThis.__sveltekit_16bkiri)==null?void 0:i.assets)??t;export{a,t as b};
---
+++ _app/immutable/entry/start.B58kEmhw.js
@@ -0,0 +1,1 @@
+import{a as t}from"../chunks/entry._o7LgEoq.js";export{t as start};
